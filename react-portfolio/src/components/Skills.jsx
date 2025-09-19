6de41f2ba405728857317b3d3a102fe1a4efbf27
Adrian Clifford Evangelio <adrianclifford.evangelio@gmail.com>
2025-09-19 23:38:35 +0800

    Added Skills.jsx

---
+++ react-portfolio/src/components/Skills.jsx
@@ -0,0 +1,15 @@
+
+function Connect() {
+    return (
+        <div className="relative overflow-hidden bg-bg-light m-6 p-4 rounded-3xl dark:bg-container-dark">
+            <h2 className="flex align-center m-4 text-4xl">Skills</h2>
+            <div className="flex flex-col items-center">
+                <h2>Front-End Development</h2>
+                <h2>Back-End Development</h2>
+                <h2>UI/UX Design</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Connect
