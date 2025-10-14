d62e37091c5859f196be0bdf2fb1d3b8d3bf460e
Adrian Clifford Evangelio <adrianclifford.evangelio@gmail.com>
2025-10-14 22:53:40 +0800

    Update content

--- react-portfolio/src/components/Content.jsx
+++ react-portfolio/src/components/Content.jsx
@@ -1,9 +1,16 @@
 export default function Content() {
   return (
     
+    <>
+      <div className="flex-1 bg-gray-100 overflow-y-auto m-6 rounded-3xl dark:bg-container-dark">
+          <h1>Welcome to my Portfolio!</h1>
+          <p>Aspiring <bold>Web Developer</bold></p>
+      </div>
 
-    <div className="flex-1 bg-gray-100 overflow-y-auto m-6 rounded-3xl dark:bg-container-dark">
-
-    </div>
+      <div className="flex-1 bg-gray-100 overflow-y-auto m-6 rounded-3xl dark:bg-container-dark">
+        
+      </div>
+    </>
+    
   )
 }
